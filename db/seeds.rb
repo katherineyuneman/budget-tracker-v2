9c258fed850ed9064038cc3fa1a0c67777c89fd7
katherineyuneman <kyuneman@gmail.com>
2022-12-06 00:21:04 -0500

    Updates seeds.rb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ##Create Months
-Month.create(month_num: 03, month_desc: "March", year: 2023)
-Month.create(month_num: 04, month_desc: "April", year: 2023)
-Month.create(month_num: 05, month_desc: "May", year: 2023)
+march23 = Month.create(month_num: 03, month_desc: "March", year: 2023)
+april23 = Month.create(month_num: 04, month_desc: "April", year: 2023)
+may23 = Month.create(month_num: 05, month_desc: "May", year: 2023)
 
 
 ### Create Users
 jane = User.create(first_name: "Jane", last_name: "Doe", email: "jane@jane.com", password_digest:'1234')
 
 
-
-
-
-
-t.string "month_num"
-    t.string "month_desc"
-    t.integer "year"
-
 ### Create Budgets
-may_budget = kat.budgets.create(amount: 2000.00, month_id: december22.id)
-june_budget = kat.budgets.create(amount: 2400.00, month_id: "June")
+march_budget = kat.budgets.create(amount: 2000.00, month_id: march23.id)
+april_budget = kat.budgets.create(amount: 2400.00, month_id: april23.id)
 
 ### Create Categories
 groceries = Category.create(description: "groceries")
@@ -35,8 +27,8 @@ clothing = Category.create(description: "shopping")
 
 
 ### Create Transactions
-may_budget.transactions.create(description: "whole foods groceries", amount: 58.20, date_created: "2022-05-01", category_id: 13)
-may_budget.transactions.create(description: "may rent", amount: 1750.00, date_created: "2022-05-02", category_id: 14)
-may_budget.transactions.create(description: "trader joes groceries", amount: 66.25, date_created: "2022-05-08", category_id: 13)
-june_budget.transactions.create(description: "june rent", amount: 1750.00, date_created: "2022-06-01", category_id: 14)
+march_budget.transactions.create(description: "whole foods groceries", amount: 58.20, date_created: "2022-12-01", category_id: 13)
+march_budget.transactions.create(description: "may rent", amount: 1750.00, date_created: "2022-12-02", category_id: 14)
+march_budget.transactions.create(description: "trader joes groceries", amount: 66.25, date_created: "2022-12-08", category_id: 13)
+april_budget.transactions.create(description: "june rent", amount: 1750.00, date_created: "2022-12-01", category_id: 14)
 
